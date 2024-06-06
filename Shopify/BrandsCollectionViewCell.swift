8da0c16fe69f79f8fb8b44c2e664ce0be008023f
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-06 13:34:34 -0700

    edit in BrandsCollectionViewCell

---
+++ Shopify/BrandsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BrandsCollectionViewCell.swift
+//  Shopify
+//
+//  Created by Mina Emad on 06/06/2024.
+//
+
+import UIKit
+
+class BrandsCollectionViewCell: UICollectionViewCell {
+    
+}
