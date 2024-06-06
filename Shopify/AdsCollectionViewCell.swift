71f9eabdf846144fbc6121099b222ac35fded6fe
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-06 11:44:46 -0700

    create AdsCollectionViewCell

---
+++ Shopify/AdsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AdsCollectionViewCell.swift
+//  Shopify
+//
+//  Created by Mina Emad on 06/06/2024.
+//
+
+import UIKit
+
+class AdsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
