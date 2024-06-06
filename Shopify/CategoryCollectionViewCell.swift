aeaa31f90d2fdc1843f9d3c3386d856842210368
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-06 12:50:30 -0700

    create CategoryCollectionViewCell

---
+++ Shopify/CategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Shopify
+//
+//  Created by Mina Emad on 06/06/2024.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
