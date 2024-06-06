a86dbf112950d10214951265b9a77924213cc6aa
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-06 11:47:15 -0700

    create BrandsCollectionViewCell

---
+++ Shopify/BrandsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BrandsCollectionViewCell.swift
+//  Shopify
+//
+//  Created by Mina Emad on 06/06/2024.
+//
+
+import UIKit
+
+class BrandsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
