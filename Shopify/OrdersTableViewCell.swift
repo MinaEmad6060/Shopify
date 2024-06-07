5b8133018a7c9c9867e26836b5457d1c4d4b97c9
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-07 06:13:04 -0700

    create OrderViewCell & WishListViewCell

---
+++ Shopify/OrdersTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrdersTableViewCell.swift
+//  Shopify
+//
+//  Created by Mina Emad on 07/06/2024.
+//
+
+import UIKit
+
+class OrdersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
