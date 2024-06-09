b3da16e4a41062d89d2a8be542d525b0acccb973
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-06-09 04:40:18 +0300

    edit network manager func

--- Shopify/Modules/Settings/viewModel/NetworkManager.swift
+++ Shopify/Modules/Settings/viewModel/NetworkManager.swift
@@ -8,16 +8,16 @@
 import Foundation
 import Alamofire
 
-class NetworkManager{
+class NetworkManager {
     
-    static func addNewAddress(customerID: Int, address: Address) {
+    static func addNewAddress(customerID: Int, address: Address, completion: @escaping (Bool) -> Void) {
         let url = "https://\(Constants.api_key):\(Constants.password)@\(Constants.hostname)/admin/api/2023-04/customers/\(customerID)/addresses.json"
         
         let customerAddressRequest = CustomerAddressRequest(address: address)
         
-        
         guard let encodedCredentials = "\(Constants.api_key):\(Constants.password)".data(using: .utf8)?.base64EncodedString() else {
             print("Failed to encode credentials")
+            completion(false)
             return
         }
         
@@ -28,12 +28,13 @@ class NetworkManager{
         
         AF.request(url, method: .post, parameters: customerAddressRequest, encoder: JSONParameterEncoder.default, headers: headers)
             .validate()
-            .responseJSON { response in
+            .response { response in
                 switch response.result {
-                case .success(let value):
-                    print("Response JSON: \(value)")
+                case .success:
+                    completion(true)
                 case .failure(let error):
                     print("Error adding address: \(error)")
+                    completion(false)
                 }
             }
     }
