5c32ba9daca21d508beaa1a47188776d5895d252
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-06-09 03:50:59 +0300

    add new address view model

--- Shopify/Modules/Settings/view/AddNewAddress.swift
+++ Shopify/Modules/Settings/view/AddNewAddress.swift
@@ -27,18 +27,7 @@ class AddNewAddress: UIViewController {
     @IBOutlet weak var addAdress: UIButton!
     
     @IBAction func addAddressBtn(_ sender: Any) {
+        
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ Shopify/Modules/Settings/viewModel/AddNewAddressViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddNewAddressViewModel.swift
+//  Shopify
+//
+//  Created by Slsabel Hesham on 09/06/2024.
+//
+
+import Foundation
