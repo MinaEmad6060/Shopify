ded00c3616dd462ffbf1a0962e907efcb5e9dea4
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-06-09 04:10:56 +0300

    add constants class

---
+++ Shopify/Constants/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Shopify
+//
+//  Created by Slsabel Hesham on 09/06/2024.
+//
+
+import Foundation
+
+class Constants{
+    
+    static var api_key = "106ef29b5ab2d72aa0243decb0774101"
+    static var password = "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"
+    static var hostname = "mad44-alex-ios-team3.myshopify.com"
+    
+}
