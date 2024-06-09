acc0636d19ab1fe148247cf38b17b1ec5d89e710
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-06-09 04:46:14 +0300

    edit constants

--- Shopify/Constants/Constants.swift
+++ Shopify/Constants/Constants.swift
@@ -9,8 +9,5 @@ import Foundation
 
 class Constants{
     
-    static var api_key = "106ef29b5ab2d72aa0243decb0774101"
-    static var password = "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"
-    static var hostname = "mad44-alex-ios-team3.myshopify.com"
     
 }
