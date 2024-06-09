183c2088f3c01781438ff16572f9e00289f2f7c2
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-06-09 04:40:58 +0300

    add address view model implementation

--- Shopify/Modules/Settings/viewModel/AddNewAddressViewModel.swift
+++ Shopify/Modules/Settings/viewModel/AddNewAddressViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class AddNewAddressViewModel {
+    var bindResultToNewAddressViewController: (() -> Void) = {}
+    var success: Bool = false {
+        didSet {
+            bindResultToNewAddressViewController()
+        }
+    }
+
+    func addNewAddress(customerId: Int, country: String, city: String, address: String, phone: String) {
+        let newAddress = Address(country: country, city: city, address: address, phone: phone)
+        
+        NetworkManager.addNewAddress(customerID: customerId, address: newAddress) { [weak self] success in
+            self?.success = success
+        }
+    }
+}
