2494f8750363cec444883c1ede21fc7432278bdc
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-10 12:39:41 -0700

    create ProductInfoViewModel

---
+++ Shopify/Modules/ProductInfo/ViewModel/ProductInfoViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ProductInfoViewModel.swift
+//  Shopify
+//
+//  Created by maha on 10/06/2024.
+//
+
+import Foundation
+class ProdutInfoViewModel {
+    var product : BrandProduct?
+    init(product: BrandProduct?) {
+        self.product = product
+    }
+    
+}
