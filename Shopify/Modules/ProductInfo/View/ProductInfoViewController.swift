4a8fc6857f227a3284afb038f40a2ca9f2c09544
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-10 00:36:45 -0700

    implement ui for product image using image slider

--- Shopify/Modules/ProductInfo/View/ProductInfoViewController.swift
+++ Shopify/Modules/ProductInfo/View/ProductInfoViewController.swift
@@ -6,24 +6,55 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class ProductInfoViewController: UIViewController {
 
+    @IBOutlet weak var imageSlideshow: ImageSlideshow!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureImageSlideshow()
     }
     
+    private func configureImageSlideshow() {
+           // Set up the images
+           let imageInputs = [
+               ImageSource(image: UIImage(named: "wish")!),
+               ImageSource(image: UIImage(named: "category")!),
+               ImageSource(image: UIImage(named: "Ad")!)
+           ]
+           
+           imageSlideshow.setImageInputs(imageInputs)
+           
+           // Optional configurations
+           imageSlideshow.slideshowInterval = 5.0
+           imageSlideshow.pageIndicatorPosition = .init(horizontal: .center, vertical: .under)
+           imageSlideshow.contentScaleMode = UIView.ContentMode.scaleAspectFill
+           
+           let pageIndicator = UIPageControl()
+           pageIndicator.currentPageIndicatorTintColor = UIColor.lightGray
+           pageIndicator.pageIndicatorTintColor = UIColor.black
+           imageSlideshow.pageIndicator = pageIndicator
+           
+           imageSlideshow.activityIndicator = DefaultActivityIndicator()
+           imageSlideshow.delegate = self
+        
+           let recognizer = UITapGestureRecognizer(target: self, action: #selector(didTap))
+           imageSlideshow.addGestureRecognizer(recognizer)
+        
+              imageSlideshow.layer.cornerRadius = 20
+              imageSlideshow.clipsToBounds = true
+       }
 
-    /*
-    // MARK: - Navigation
+       @objc func didTap() {
+           let fullScreenController = imageSlideshow.presentFullScreenController(from: self)
+           fullScreenController.slideshow.activityIndicator = DefaultActivityIndicator(style: .white, color: nil)
+       }
+   }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+   extension ProductInfoViewController: ImageSlideshowDelegate {
+       func imageSlideshow(_ imageSlideshow: ImageSlideshow, didChangeCurrentPageTo page: Int) {
+           print("current page:", page)
+       }
+   }
