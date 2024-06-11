f1384e661c8777367d9437f7bd65d667b41db4dc
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-11 00:17:24 -0700

    create checkCustomerAuth function

--- Shopify/Modules/Login/ViewModel/LoginViewModel.swift
+++ Shopify/Modules/Login/ViewModel/LoginViewModel.swift
@@ -16,8 +16,21 @@ class LoginViewModel{
     }
     
     func getAllCustomers() {
-            apiService.getSportData(url: apiService.formatUrl(request: "customers"), handler: { [weak self] (customers: LoginedCustomers) in
-                self?.observableLogin = customers
-            })
+        apiService.getSportData(url: apiService.formatUrl(request: "customers"), handler: { [weak self] (customers: LoginedCustomers) in
+            self?.observableLogin = customers
+        })
+    }
+    func checkCustomerAuth(email:String , password: String)->String{
+        var returnValue = "Uncorrect email or password"
+        if let observale = observableLogin{
+            print(observale.customers.count)
+            for i in 0..<(observale.customers.count){
+                if email == observale.customers[i].email && password == observale.customers[i].tags{
+                    returnValue = "Login Sucess"
+                }
+                
+            }
         }
+       return returnValue
+    }
 }
