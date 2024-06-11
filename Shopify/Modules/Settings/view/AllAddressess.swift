b242de082b95813473e29835f7d7c8bd30eb011c
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-06-11 04:23:32 +0300

    all addressess view controller

---
+++ Shopify/Modules/Settings/view/AllAddressess.swift
@@ -0,0 +1,30 @@
+//
+//  AllAddressess.swift
+//  Shopify
+//
+//  Created by Slsabel Hesham on 11/06/2024.
+//
+
+import UIKit
+
+class AllAddressess: UIViewController {
+
+    @IBOutlet weak var allAddressessTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
