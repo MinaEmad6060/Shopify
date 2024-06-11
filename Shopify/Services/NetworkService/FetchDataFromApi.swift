4c3e5ec9eb9c258f01c4577bc12c41f195880e17
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-11 15:23:58 -0700

    delete extra code

--- Shopify/Services/NetworkService/FetchDataFromApi.swift
+++ Shopify/Services/NetworkService/FetchDataFromApi.swift
@@ -12,7 +12,7 @@ import Alamofire
 class FetchDataFromApi{
     
     var baseUrl = "https://106ef29b5ab2d72aa0243decb0774101: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@mad44-alex-ios-team3.myshopify.com/admin/api/2024-04/"
-    //smart_collections.json
+
     func formatUrl(request: String, query: String="", value: String="") -> String{
         return baseUrl+request+".json?"+query+"="+value
     }
