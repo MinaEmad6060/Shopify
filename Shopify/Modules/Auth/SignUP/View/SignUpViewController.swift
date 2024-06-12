e9b49ec20ca4709aaa0c931006a7780b0d265110
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-12 01:14:32 -0700

    add outlets for signup

--- Shopify/Modules/Auth/SignUP/View/SignUpViewController.swift
+++ Shopify/Modules/Auth/SignUP/View/SignUpViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+   
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+  
+    
+    @IBOutlet weak var lnameTextField: UITextField!
+    
+    @IBOutlet weak var fnameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
