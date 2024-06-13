e35f679877a6873cfe1650f31365768380f7c1ad
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-13 12:42:19 -0700

    create func createDraft in service

--- Shopify/Services/NetworkService/FetchDataFromApi.swift
+++ Shopify/Services/NetworkService/FetchDataFromApi.swift
@@ -31,5 +31,62 @@ class FetchDataFromApi{
             }
         }
     }
-    
+    static func createDraft(product: Product, note: String, complication: @escaping (Int) -> Void) {
+        let urlString = "https://106ef29b5ab2d72aa0243decb0774101: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@mad44-alex-ios-team3.myshopify.com/admin/api/2024-04/draft_orders.json"
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        
+        let lineItem: [String: Any] = [
+            "id": product.id ?? 0,
+            "title": product.title ?? "",
+            "quantity": 2,
+            "price": product.variants?.first?.price ?? "20",
+            "sku": "\(product.id ?? 0),\((product.images?[0]) )"
+        ]
+        
+        
+      
+        let draftOrder: [String: Any] = [
+            "note": note,
+            "line_items": [lineItem],
+          
+            
+        ]
+        
+        let userDictionary: [String: Any] = [
+            "draft_order": draftOrder
+        ]
+        
+        do {
+            let bodyData = try JSONSerialization.data(withJSONObject: userDictionary, options: .prettyPrinted)
+            urlRequest.httpBody = bodyData
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        } catch {
+            print("Error serializing JSON: \(error.localizedDescription)")
+            return
+        }
+
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+
+            if let httpResponse = response as? HTTPURLResponse {
+                if let data = data, let responseString = String(data: data, encoding: .utf8) {
+                    print(responseString)
+                }
+                complication(httpResponse.statusCode)
+            }
+        }.resume()
+    }
+
 }
+
+    
+   
