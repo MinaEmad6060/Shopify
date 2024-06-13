be7a96508326cbd855f3d06e678df09a302097fa
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-12 23:07:48 -0700

    add  validation for email and password

--- Shopify/Modules/Auth/SignUP/View/SignUpViewController.swift
+++ Shopify/Modules/Auth/SignUP/View/SignUpViewController.swift
@@ -23,7 +23,11 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         signUpViewModel = SignUpViewModel()
-        
+        signUpViewModel?.bindingSignUp = {
+                    DispatchQueue.main.async {
+                        self.handleSignUpResponse()
+                    }
+                }
     }
     
 
@@ -39,10 +43,12 @@ class SignUpViewController: UIViewController {
             print("customer is nullll*")
                   return
               }
+        /*
         if fnameTextField.text != "" && lnameTextField.text != "" && emailTextField.text != "" && passwordTextField.text != "" && confirmPasswordTextField.text != "" {
                    
             if signUpViewModel?.customer.tags == checkConfirmPassword {
                       signUpViewModel?.addCustomer(customer: customer)
+               
                    }
                    else{
                        Utilites.displayToast(message: "Confirm Password and Password must be identical", seconds: 2.0, controller: self)
@@ -51,9 +57,60 @@ class SignUpViewController: UIViewController {
                }else{
                    Utilites.displayToast(message: "Enter Full Data", seconds: 2.0, controller: self)
                    print("Enter Full Data")
-               }
+               }*/
        
-               
+        if let email = emailTextField.text, let password = passwordTextField.text, let confirmPassword = confirmPasswordTextField.text, let firstName = fnameTextField.text, let lastName = lnameTextField.text, !email.isEmpty, !password.isEmpty, !confirmPassword.isEmpty, !firstName.isEmpty, !lastName.isEmpty {
+                   
+                   if isValidEmail(email) {
+                       if isValidPassword(password) {
+                           if password == confirmPassword {
+                               signUpViewModel?.addCustomer(customer: customer)
+                               let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as! LoginViewController
+                                                       loginViewController.modalPresentationStyle = .fullScreen
+                                                       self.navigationController?.present(loginViewController, animated: true)
+                                                   
+                           } else {
+                               Utilites.displayToast(message: "Confirm Password and Password must be identical", seconds: 2.0, controller: self)
+                               print("Confirm Password and Password must be identical")
+                           }
+                       } else {
+                           Utilites.displayToast(message: "Password must be at least 8 characters long and contain a number and a special character", seconds: 2.0, controller: self)
+                           print("Password must be at least 8 characters long and contain a number and a special character")
+                       }
+                   } else {
+                       Utilites.displayToast(message: "Invalid email format", seconds: 2.0, controller: self)
+                       print("Invalid email format")
+                   }
+               } else {
+                   Utilites.displayToast(message: "Enter Full Data", seconds: 2.0, controller: self)
+                   print("Enter Full Data")
+               }
+           }
+           
+    func isValidEmail(_ email: String) -> Bool {
+          let emailRegEx = "^[A-Z0-9a-z._%+-]+@[A-Z0-9a-z.-]+\\.[A-Za-z]{2,64}$"
+          let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+          return emailTest.evaluate(with: email)
+      }
+           
+           func isValidPassword(_ password: String) -> Bool {
+               // Minimum 8 characters, at least 1 letter, 1 number and 1 special character
+               let passwordRegEx = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
+               let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+               return passwordTest.evaluate(with: password)
+           
     }
-    
+    private func handleSignUpResponse() {
+            guard let statusCode = signUpViewModel?.ObservableSignUp else { return }
+            
+            if (200...299).contains(statusCode) {
+                print("Navigating to LoginVC")
+                let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as! LoginViewController
+                loginViewController.modalPresentationStyle = .fullScreen
+                self.present(loginViewController, animated: true, completion: nil)
+            } else {
+                Utilites.displayToast(message: "Failed to add customer", seconds: 2.0, controller: self)
+                print("Failed to add customer, status code: \(statusCode)")
+            }
+        }
 }
--- Shopify/Modules/Auth/SignUP/ViewModel/SignUpViewModel.swift
+++ Shopify/Modules/Auth/SignUP/ViewModel/SignUpViewModel.swift
@@ -23,4 +23,5 @@ class SignUpViewModel{
             self.ObservableSignUp = checkSignAblitiy
         }
         }
+    
 }
