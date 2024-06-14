f8f145df79f0a21298ef3abdfc2426f0685f941e
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-14 09:52:59 -0700

    store customer id and email in user default

--- Shopify/Modules/Auth/Login/ViewModel/LoginViewModel.swift
+++ Shopify/Modules/Auth/Login/ViewModel/LoginViewModel.swift
@@ -21,17 +21,48 @@ class LoginViewModel{
             self?.observableLogin = customers
         })
     }
-    func checkCustomerAuth(email:String , password: String)->String{
-        var returnValue = "Uncorrect email or password"
-        if let observale = observableLogin{
-            print(observale.customers.count)
-            for i in 0..<(observale.customers.count){
-                if email == observale.customers[i].email && password == observale.customers[i].tags{
-                    returnValue = "Login Sucess"
-                }
-                
-            }
-        }
-       return returnValue
-    }
+//    func checkCustomerAuth(email:String , password: String)->String{
+//        var returnValue = "Uncorrect email or password"
+//        if let observale = observableLogin{
+//            print(observale.customers.count)
+//            for i in 0..<(observale.customers.count){
+//                if email == observale.customers[i].email && password == observale.customers[i].tags{
+//                    returnValue = "Login Sucess"
+//                    
+//                }
+//                
+//            }
+//        }
+//       return returnValue
+//    }
+    func checkCustomerAuth(email: String, password: String) -> String {
+           var returnValue = "Incorrect email or password"
+           
+           if let observable = observableLogin {
+               for customer in observable.customers {
+                   if email == customer.email && password == customer.tags {
+                       // Save user ID and email to UserDefaults
+                       UserDefaults.standard.set(customer.id, forKey: "userID")
+                       UserDefaults.standard.set(customer.email, forKey: "userEmail")
+                       returnValue = "Login Success"
+                       
+                       // Print stored user info
+                       printStoredUserInfo()
+                       break
+                   }
+               }
+           }
+           
+           return returnValue
+       }
+       
+       func printStoredUserInfo() {
+           if let userID = UserDefaults.standard.string(forKey: "userID"),
+              let userEmail = UserDefaults.standard.string(forKey: "userEmail") {
+               print("User ID: \(userID)")
+               print("User Email: \(userEmail)")
+           } else {
+               print("No user information found in UserDefaults.")
+           }
+       }
 }
