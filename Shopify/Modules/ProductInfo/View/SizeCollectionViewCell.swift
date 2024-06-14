31cfa3d7281a0b9ced360ce5fecf866aafab670b
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-13 17:44:03 -0700

    create sizeCollectionView

---
+++ Shopify/Modules/ProductInfo/View/SizeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SizeCollectionViewCell.swift
+//  Shopify
+//
+//  Created by maha on 13/06/2024.
+//
+
+import UIKit
+
+class SizeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var sizeLB: UILabel!
+    
+    @IBOutlet weak var sizeView: UIView!
+}
