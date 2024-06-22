7fdf0e7e118565feab2d866505d11e78628db82e
Maha Farghaly <mahafarghalymo@gmail.com>
2024-06-21 19:54:07 -0700

    add ReviewViewController

---
+++ Shopify/Modules/Review/View/ReviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReviewViewController.swift
+//  Shopify
+//
+//  Created by maha on 21/06/2024.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    @IBOutlet weak var reviewTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
