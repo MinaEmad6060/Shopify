24ecad13e06bf9e7465cfc56724458b8185188b2
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-06-26 02:43:51 +0300

    edit in order summary

--- Shopify/Modules/Settings/view/PlaceOrderViewController.swift
+++ Shopify/Modules/Settings/view/PlaceOrderViewController.swift
@@ -6,20 +6,37 @@
 //
 
 import UIKit
+import PassKit
 
 class PlaceOrderViewController: UIViewController {
     
+    
+    private var payment : PKPaymentRequest = PKPaymentRequest()
+    
+    @IBAction func changePayment(_ sender: Any) {
+        let payment = UIStoryboard(name: "Payment", bundle: nil).instantiateViewController(withIdentifier: "PaymentOptionsVC") as! PaymentOptionsViewController
+        present(payment, animated: true)
+    }
+    @IBOutlet weak var paymentLabel: UILabel!
+    @IBOutlet weak var changeAddress: UIButton!
+    var allAddressesViewModel: AllAddressesViewModel?
+    
     @IBAction func changeAddress(_ sender: Any) {
-        let addressess = UIStoryboard(name: "Settings", bundle: nil).instantiateViewController(withIdentifier: "addresses") as! AllAddressess
-        present(addressess, animated: true)
+        if self.changeAddress.titleLabel?.text == "Change"{
+            let addressess = UIStoryboard(name: "Settings", bundle: nil).instantiateViewController(withIdentifier: "addresses") as! AllAddressess
+            present(addressess, animated: true)
+        } else{
+            let addAddress = UIStoryboard(name: "Settings", bundle: nil).instantiateViewController(withIdentifier: "addAddress") as! AddNewAddress
+            present(addAddress, animated: true)
+        }
     }
     @IBAction func backBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     @IBOutlet weak var couponErrorLabel: UILabel!
     var lineItems: [LineItemm] = []
-//    var lineItemsTest: [LineItemm] = []
-//    var ItemTest: LineItemm?
+    //    var lineItemsTest: [LineItemm] = []
+    //    var ItemTest: LineItemm?
     var subTotal = 0.0
     var total = 0.0
     let customer: [String: Any] = [
@@ -27,7 +44,7 @@ class PlaceOrderViewController: UIViewController {
         "currency": "EGP"
     ]
     @IBOutlet weak var couponTF: UITextField!
- 
+    
     @IBAction func validateBtn(_ sender: Any) {
         guard let couponCode = couponTF.text, !couponCode.isEmpty else {
             print("Please enter a coupon code")
@@ -36,28 +53,74 @@ class PlaceOrderViewController: UIViewController {
         
         validateDiscountCode(couponCode)
     }
+    @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var discountAmountLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var subTotalLabel: UILabel!
+    override func viewWillAppear(_ animated: Bool) {
+        self.paymentLabel.text = "Payment: \(Utilites.getPaymentMethod())"
+        allAddressesViewModel?.getAllAddress(customerId: Utilites.getCustomerID())    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        allAddressesViewModel = AllAddressesViewModel()
+        allAddressesViewModel?.bindResultToAllAddressViewController = { [weak self] in
+            DispatchQueue.main.async {
+                if self?.allAddressesViewModel?.addresses.count == 0{
+                    self?.changeAddress.titleLabel?.text = "Set"
+                }else{
+                    var addresses: [AddressId] = []
+                    addresses = (self?.allAddressesViewModel!.addresses)!
+                    for address in addresses{
+                        if address.default == true {
+                            print("testttttttt")
+                            self?.addressLabel.text = address.address1
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+        
+        
         print(self.lineItems)
         // Do any additional setup after loading the view.
         self.subTotalLabel.text = "\(self.subTotal)EGP"
         self.discountAmountLabel.text = "-0.EGP"
         self.totalLabel.text = "\(self.subTotal)EGP"
-                
-//        lineItemsTest = [
-//            LineItemm(id: 8100172660907, title: "SUPRA | MENS VAIDER", quantity: 1, price: "\(169.95)", variant_id: "", variant_title: ""),
-//            LineItemm(id: 8100172595371, title: "PUMA | SUEDE CLASSIC REGAL", quantity: 2, price: "\(110.00)", variant_id: "", variant_title: ""),
-//        ]
+        
+        payment.merchantIdentifier = "merchant.com.pushpendra.pay"
+        payment.supportedNetworks = [.quicPay, .masterCard, .visa]
+        payment.supportedCountries = ["EG", "US"]
+        payment.merchantCapabilities = .capability3DS
+        payment.countryCode = "EG"
+        payment.currencyCode = Utilites.getCurrencyCode()
+        
     }
     
     @IBAction func placeOrderBtn(_ sender: Any) {
         print("placeOrderBtnCount :: \(lineItems.count)")
-        FetchDataFromApi.postOrder(lineItems: lineItems, customer: customer)
-        
+        if self.addressLabel.text != "No Address Set" && Utilites.getPaymentMethod() == "Cash"{
+            print("1")
+            FetchDataFromApi.postOrder(lineItems: lineItems, customer: customer)
+        } else if self.addressLabel.text == "No Address Set" {
+            print("2")
+
+            Utilites.displayToast(message: "Please set your address first", seconds: 2.0, controller: self)
+        } else {
+            print("3")
+
+            let amount = String(self.subTotal)
+            payment.paymentSummaryItems = [PKPaymentSummaryItem(label: "iPhone XR 128 GB", amount: NSDecimalNumber(string: amount))]
+            
+            let controller = PKPaymentAuthorizationViewController(paymentRequest: payment)
+            if let paymentController = controller {
+                //paymentController.delegate = paymentController
+                present(paymentController, animated: true, completion: nil)
+            }
+            
+        }
     }
 
     /*
@@ -86,9 +149,7 @@ class PlaceOrderViewController: UIViewController {
                 useDiscountCode(code)
                 print("Discount code applied successfully!")
                 
-                // جلب قيمة الخصم
                 if let discountValue = Double(discountCode.value) {
-                    // استخدم القيمة المطلقة للخصم
                     let absoluteDiscountValue = abs(discountValue)
                     let discountAmount = calculateDiscount(totalPrice: subTotal, discountValue: absoluteDiscountValue)
                     let newTotalPrice = subTotal - discountAmount
@@ -112,7 +173,6 @@ class PlaceOrderViewController: UIViewController {
     }
 
     func calculateDiscount(totalPrice: Double, discountValue: Double) -> Double {
-        // تفترض أن قيمة الخصم كنسبة مئوية (على سبيل المثال: 20 تعني 20% خصم)
         return totalPrice * (discountValue / 100.0)
     }
         
@@ -128,4 +188,15 @@ class PlaceOrderViewController: UIViewController {
             UserDefaults.standard.removeObject(forKey: "SelectedDiscountCode")
         }
         
+}
+extension PaymentOptionsViewController : PKPaymentAuthorizationViewControllerDelegate {
+    
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+            controller.dismiss(animated: true) { [weak self] in
+                // animation and navigate to home
+            }
+        }
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+    }
 }
