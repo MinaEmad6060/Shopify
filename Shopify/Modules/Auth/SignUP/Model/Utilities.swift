7a1ea23533bd7f155e8bbdeecf6862c6d5117944
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-06-26 02:42:20 +0300

    get Curruncy info utilities method

--- Shopify/Modules/Auth/SignUP/Model/Utilities.swift
+++ Shopify/Modules/Auth/SignUP/Model/Utilities.swift
@@ -8,38 +8,48 @@
 import Foundation
 import SystemConfiguration
 import UIKit
+
 class Utilites{
-   static func displayToast(message : String, seconds: Double, controller: UIViewController){
-            
-            let alert = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+
+    static func displayToast(message : String, seconds: Double, controller: UIViewController){
+        
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
         alert.view.backgroundColor = UIColor.brown
-            alert.view.layer.cornerRadius = 15
-            controller.present(alert, animated: true)
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
-                alert.dismiss(animated: true)
-            }
+        alert.view.layer.cornerRadius = 15
+        controller.present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
         }
-   static func getDraftOrderIDCart() -> Int {
+    }
+    static func getDraftOrderIDCart() -> Int {
         return UserDefaults.standard.integer(forKey: "draftOrderIDCart")
     }
-
-    
-    static  func getDraftOrderIDFavorite() -> Int {
+    static func getDraftOrderIDFavorite() -> Int {
         return UserDefaults.standard.integer(forKey: "draftOrderIDFavorite")
     }
-    static  func getCustomerID() -> Int{
+    static func getCustomerID() -> Int{
         return UserDefaults.standard.integer(forKey: "userID")
     }
-    static  func getCustomerEmail() -> String{
+    static func getCustomerEmail() -> String{
         return UserDefaults.standard.string(forKey: "userEmail") ?? "Guest"
     }
-    static  func getDraftOrderIDFavoriteFromNote() -> Int {
+    static func getDraftOrderIDFavoriteFromNote() -> Int {
         return UserDefaults.standard.integer(forKey: "favIDNet")
     }
-    static  func getDraftOrderIDCartFromNote() -> Int {
+    static func getDraftOrderIDCartFromNote() -> Int {
         return UserDefaults.standard.integer(forKey: "cartIDNet")
     }
-    static  func getCustomerName() -> String {
+    static func getCustomerName() -> String {
         return UserDefaults.standard.string(forKey: "fname") ?? "non"
+    }
+    
+    static func getCurrencyCode() -> String {
+        return UserDefaults.standard.string(forKey: "selectedCurrencyCode") ?? "EGP"
+    }
+    static func getCurrencyRate() -> String {
+        return UserDefaults.standard.string(forKey: "selectedCurrencyRate") ?? "1"
+    }
+    static func getPaymentMethod() -> String {
+        return UserDefaults.standard.string(forKey: "paymentMethod") ?? "Cash"
     }
 }
